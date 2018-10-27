681fad53a8dc8fbe9dc16bec671e5ff3cdecc509
gegelascience <remi.verchere2@gmail.com>
2018-10-27 17:51:08 +0200

    unused files

--- src/router.js
+++ src/router.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
-import AframeScene from './views/AframeScene.vue';
 
 Vue.use(Router)
 
@@ -20,18 +19,12 @@ export default new Router({
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/EiffelTower.vue')
+      component: () => import( './views/EiffelTower.vue')
     },
     {
       path:'/solarSys',
       name:'solarSys',
       component: () => import('./views/SolarSystem.vue')
-    },
-    {
-      path:'/scene/:id',
-      name:'aframeScene',
-      component: AframeScene,
-      props:true
     }
   ]
 })
